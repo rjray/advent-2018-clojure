9644a12e921bd87187d018abe395ccde2553b167
Randy J. Ray <rjray@blackperl.com>
2018-12-08 11:57:05 -0800

    Added the lein-kibit plugin.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://adventofcode.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-kibit "0.1.6"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/math.combinatorics "0.1.4"]])
